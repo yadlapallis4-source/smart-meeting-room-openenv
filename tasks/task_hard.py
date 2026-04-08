bf786e3ca8638f527f594542d97c99a4bde1dcc7
yadlapallis4-source <yadlapallis4@gmail.com>
2026-04-08 20:02:39 +0530

    final fix remove get_task

--- tasks/task_hard.py
+++ tasks/task_hard.py
@@ -1,13 +1,2 @@
-def get_task():
-    return {
-        "id": "task_hard",
-        "difficulty": "hard",
-        "description": "Simple",
-        "initial_state": {},
-        "action_space": ["noop()"],
-        "max_steps": 1,
-    }
-
-
 def grader(action, state):
     return 0.8
