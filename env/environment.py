1dcf450f2d4df30ce574fb890819c68dab214a18
yadlapallis4-source <yadlapallis4@gmail.com>
2026-04-10 09:13:02 +0530

    final fix: correct task mapping

--- env/environment.py
+++ env/environment.py
@@ -26,25 +26,24 @@ class MeetingRoomEnv:
         }
         return dict(self._state)
 
-    def step(self, action=None):
-        if self._state is None:
-            self.reset()
-
-        self._state["step_count"] += 1
-        self._state["last_action"] = action
-
-        if self.task_type == "easy":
-            reward = easy_grader(action, self._state)
-        elif self.task_type == "medium":
-            reward = medium_grader(action, self._state)
-        else:
-            reward = hard_grader(action, self._state)
-
-        done = True
-        state = dict(self._state)
-        state.pop("done", None)
-
-        return state, float(reward), done, {}
-
-    def state(self):
-        return dict(self._state) if self._state is not None else None
+    def get_tasks(self):
+        return [
+            {
+                "id": "task_easy",
+                "description": "Select room for 4 people",
+                "difficulty": "easy",
+                "grader": easy_grader,
+            },
+            {
+                "id": "task_medium",
+                "description": "Select room with projector",
+                "difficulty": "medium",
+                "grader": medium_grader,
+            },
+            {
+                "id": "task_hard",
+                "description": "Select optimal room",
+                "difficulty": "hard",
+                "grader": hard_grader,
+            },
+        ]
