44af4249268bd6ff1bebe9fa409c65bef41881b0
yadlapallis4-source <yadlapallis4@gmail.com>
2026-04-10 10:53:24 +0530

    added step function final fix

--- env/environment.py
+++ env/environment.py
@@ -47,3 +47,26 @@ class MeetingRoomEnv:
                 "grader": hard_grader,
             },
         ]
+
+        def step(self, action=None):
+            if self._state is None:
+                self.reset()
+
+            self._state["step_count"] += 1
+            self._state["last_action"] = action
+
+            # Apply correct grader
+            if self.task_type == "easy":
+                reward = easy_grader(action, self._state)
+            elif self.task_type == "medium":
+                reward = medium_grader(action, self._state)
+            else:
+                reward = hard_grader(action, self._state)
+
+            done = True
+
+            # Remove done from state (important)
+            state = dict(self._state)
+            state.pop("done", None)
+
+            return state, float(reward), done, {}
